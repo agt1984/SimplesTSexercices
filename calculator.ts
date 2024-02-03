72c365e9466d75589e8656855926e6dcab492905
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-02-03 12:11:17 +0100

    more cleaner code

--- calculator.ts
+++ calculator.ts
@@ -5,13 +5,15 @@ type Result    =  number                       //aqui definimos este type como n
 
 //en los parametros de la funcion colocamos tambien el tipo de operacion, atento a la sintaxis de abajo, esta funcion tiene un parametro adicional, vé el ":" y luego el Result, este se encarga de decirle a la funcion que solo atrapara y devolvera un number. Esto es considerado una buena practica, que una funcion devuelva un solo tipo de variable
 const calculator = (a: number, b: number, op: Operation): Result => {  
-    if (op == 'multiply') return a * b  //si op es igual a 'multiply' return multiplicacion de a por b
-    if (op == 'add')      return a + b  //si op es igual a 'add' return suma de a por b
+    if (op === 'multiply') return a * b  //si op es igual a 'multiply' return multiplicacion de a por b
+    if (op === 'add')      return a + b  //si op es igual a 'add' return suma de a por b
 
     if(op == 'divide') {                    
         if(a === 0 || b === 0) throw new Error('Can\t divide by 0')
         return a/b
     }
+
+    throw new Error('Operation is not valid')
 }
 
 //llamamos la funcion para que muestre el resultado por consola
